c28b0b8e2c00778e2ae9d95b3aef4d7e926984be
ansons <ansonstewart@gmail.com>
2019-11-07 15:56:13 -0500

    Add example R code
    using tidytransit

---
+++ stop-map.R
@@ -0,0 +1,35 @@
+# Install packages. sf is finicky, try installing it first.
+install.packages('sf')
+install.packages('tidyverse')
+install.packages('ggplot2')
+install.packages('tidytransit')
+
+library('sf')
+library('tidyverse')
+library('ggplot2')
+library('tidytransit')
+
+# Read GTFS
+gtfs <- read_gtfs("data/calgary-2019/2019.zip")
+gtfs <- set_date_service_table(gtfs)
+gtfs <- set_servicepattern(gtfs)
+
+date_table <- gtfs$.$date_servicepattern_table
+
+service_table <- gtfs$.$service_pattern
+
+# Paste service_id values below after inspecting two tables above
+am_freq <- get_stop_frequency(gtfs, start_hour = 7, end_hour = 8, service_ids = 
+                                c('1BUSWK-Weekday-4-2019MA-1111100', 
+                                  '1BUSWK-Weekday-4-2019MA-1111000',
+                                  '1BUSWK-Weekday-4-2019MA-0010000',
+                                  '1BUSWK-Weekday-4-2019MA-0000100'
+                                  
+))
+
+
+stops <- left_join(gtfs$stops, am_freq, by='stop_id') %>%
+  group_by(stop_id) %>%
+  transmute(total_departures=sum(departures, na.rm = TRUE))
+
+stops <- distinct(right_join(gtfs$stops, stops, by='stop_id'))
